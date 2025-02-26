85b8fd2845390c9129d02be0cecbdcd275613fe2
Nigashan <it21026720@my.sliit.lk>
2025-02-27 00:35:54 +0530

    commiting the last changes

--- components/main-header.js
+++ components/main-header.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader(){
-    return <header className={classes.header}>
+    return( 
+    <>
+    <MainHeaderBackground/>
+    <header className={classes.header}>
         <Link className={classes.logo} href='/'>
-        <img src={logoImg.src} alt='a plate with food on it' />
+        <Image src={logoImg} alt='a plate with food on it' priority />
         NextLevel food
         </Link>
 
@@ -20,4 +25,6 @@ export default function MainHeader(){
             </ul>
         </nav>
     </header>
+    </>
+    )
 }
